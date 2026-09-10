6ae1caba5a7b0f69ebe0c5a152e2c15a4e492ae3
Hector <sadkld121.clan@gmail.com>
2026-09-10 00:40:30 -0300

    adições de metodos, alterações em setters, adição de construtores e atributos

--- Pessoa.java
+++ Pessoa.java
@@ -7,6 +7,16 @@ public class Pessoa {
     private  String email;
     private  String telefone;
 
+    public  Pessoa(){}
+
+    public Pessoa( String nome, String cpf, String email, String telefone){
+        this.telefone = telefone;
+        this.email = email;
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+
+
     public Pessoa(int id, String nome, String cpf, String email, String telefone){
         this.telefone = telefone;
         this.id = id;
@@ -15,14 +25,67 @@ public class Pessoa {
         this.cpf = cpf;
     }
 
-    
+    private boolean verificarCpf(String cpf){
+        int a,b;
+        a = cpf.charAt(9) - '0';
+        b = cpf.charAt(10) - '0';
+
+        int sum = 0;
+        int d1,d2;
+        int[] cpfNum = new int[9];
+        int[] num = {10,9,8,7,6,5,4,3,2};
+        // calculo dv
+        // coleta os numeros do cpf um por um e separa cada um em uma lista.
+        for (int i = 0; i <= cpf.length()-2; i++) {
+            char k = cpf.charAt(i);
+            cpfNum[i] = k - '0';
+        }
+        // calculo bruto!
+        for(int i = 0; i <= cpfNum.length; i++){
+            sum += cpfNum[i] * num[i];
+        }
+
+        int resto = sum%11;
+        d1 = 11 - resto;
+        sum = 0;
+
+        //calculo segundo dv
+        // coleta os numeros do cpf um por um e separa cada um em uma lista.
+        for (int i = 0; i <= cpf.length()-2; i++) {
+            if(cpf.length() == 7){
+                cpfNum[i] = cpfNum[i+1];
+            }else{
+                cpfNum[8] = d1;
+            }
+        }
+        // calculo bruto!
+        for(int i = 0; i <= cpfNum.length; i++){
+            sum += cpfNum[i] * num[i];
+        }
+
+        int resto2 = sum%11;
+        d2 = 11 - resto2;
+
+        if(a != d1 || b != d2){
+            // System.out.println("O cpf é invalido!");
+            return false;
+        }else{
+            // System.out.println("O cpf é válido");
+            return true;
+        }
+
+        
+        
+    }    
 
     // getters e setters
     public void setCpf(String cpf) {
         if(cpf.trim().isEmpty()){
             System.out.println("O cpf não pode estar vazio!");
-        }else if(cpf.matches("[\\d{3}].[\\d{3}].[\\d{3}]-[\\d{2}]")){
+        }else if(cpf.matches("[\\d{3}].[\\d{3}].[\\d{3}]-[\\d{2}] || [\\d{1,11}]")){
             System.out.println("O cpf não segue o modelo correto!");
+        }else if(!verificarCpf(cpf)){
+            System.out.println("CPF invalido!");
         }else{
             this.cpf = cpf;
         }
