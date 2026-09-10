999fb05979b2c9257d00036aa5762b309411b84a
Hector <sadkld121.clan@gmail.com>
2026-09-09 23:51:48 -0300

    Setters e Getters configurados

--- Pessoa.java
+++ Pessoa.java
@@ -15,22 +15,51 @@ public class Pessoa {
         this.cpf = cpf;
     }
 
+    
 
     // getters e setters
     public void setCpf(String cpf) {
-        this.cpf = cpf;
+        if(cpf.trim().isEmpty()){
+            System.out.println("O cpf não pode estar vazio!");
+        }else if(cpf.matches("[\\d{3}].[\\d{3}].[\\d{3}]-[\\d{2}]")){
+            System.out.println("O cpf não segue o modelo correto!");
+        }else{
+            this.cpf = cpf;
+        }
     }
     public void setEmail(String email) {
-        this.email = email;
+        if(email.trim().isEmpty()){
+            System.out.println("Email não pode ser vazio!");
+        }else  if(!email.matches("^[\\w.]+@[\\w.].[\\w{2,}]&")){
+            System.out.println("Email fora de formato compativel!");
+        }else{
+            this.email = email;
+        }
     }
     public void setId(int id) {
-        this.id = id;
+        if(id <= 0){
+            System.out.println("Id impossivel!");
+        }else{
+            this.id = id;
+        }
     }
     public void setNome(String nome) {
-        this.nome = nome;
+        if(nome.trim().isEmpty()){
+            System.out.println("Nome vazio!");
+        }else if(nome.matches("(\\d+) && (\\W+) && (\\S+)")){
+            System.out.println("O nome não deve conter números ou caracteres especiais!");
+        }else{
+            this.nome = nome;
+        }
     }
     public void setTelefone(String telefone) {
-        this.telefone = telefone;
+        if(telefone.trim().isEmpty()){
+            System.out.println("O numero de telefone não pode estar vazio!");
+        }else if(!telefone.matches("\\d{2}9\\d{8}")){
+            System.out.println("Estrutura inadequada para numeros!");
+        }else{
+            this.telefone = telefone;
+        }
     }
     public String getCpf() {
         return cpf;
